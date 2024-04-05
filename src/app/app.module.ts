7065b5bbde9ec33a719bcf0256943aa1beb964f0
3bda1137 <abdallahmahfouz111@gmail.com>
2024-04-05 11:59:52 +0200

    solve Day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { NavParComponent } from './components/nav-par/nav-par.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainComponent,
+    NavParComponent
   ],
   imports: [
     BrowserModule,
